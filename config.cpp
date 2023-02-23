e68a46e6fc5a21b1e4f9b6ee822f8d8072a4c231
CommanderBeelo <xxxbilliamxxx@gmail.com>
2023-02-23 12:16:36 -0500

    Initial commit

---
+++ config.cpp
@@ -0,0 +1,47 @@
+class CfgPatches
+{
+	class MMGNoCrafting
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=1.25;
+		requiredAddons[]=
+		{
+			"DZ_Data",
+			"mmg_storage"
+		};
+	};
+};
+class CfgMods
+{
+	class MMGNoCrafting
+	{
+		dir="MMGNoCrafting";
+		picture="";
+		action="";
+		hideName=1;
+		hidePicture=1;
+		name="MMG No Crafting";
+		credits="Mighty";
+		author="Commander Beelo";
+		authorID="0";
+		version="1.0";
+		extra=0;
+		type="mod";
+		dependencies[]=
+		{
+			"World"
+		};
+		class defs
+		{
+			class worldScriptModule
+			{
+				value="";
+				files[]=
+				{
+					"MMGNoCrafting/Scripts/4_World"
+				};
+			};
+		};
+	};
+};
---
+++ scripts/4_world/unregisterbasebuilding.c
@@ -0,0 +1,18 @@
+modded class PluginRecipesManager 
+{
+    override void RegisterRecipies()
+    {
+        super.RegisterRecipies();
+        
+        UnregisterRecipe("craft_mmg_crate03_unfinished_wood");
+        UnregisterRecipe("craft_mmg_crate03_wood");
+        UnregisterRecipe("craft_mmg_crate03_grey");
+        UnregitserRecipe("craft_mmg_gun_wall_kit_wooden");
+        UnregisterRecipe("craft_mmg_gun_wall_kit_metal");
+        UnregisterRecipe("craft_mmg_gun_rack_planks");
+        UnregisterRecipe("craft_mmg_craftable_gun_rack");
+        UnregisterRecipe("craft_mmg_craftable_tent_kit");
+        UnregisterRecipe("craft_mmg_old_table");
+        UnregisterRecipe("craft_mmg_wall_shelf);");
+    }            
+}
