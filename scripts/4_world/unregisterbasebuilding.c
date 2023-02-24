2c3f68b51f1b4841f0a02547c8a74db5443e34a8
CommanderBeelo <xxxbilliamxxx@gmail.com>
2023-02-23 21:14:12 -0500

    Update unregisterbasebuilding.c

--- scripts/4_world/unregisterbasebuilding.c
+++ scripts/4_world/unregisterbasebuilding.c
@@ -1,12 +1,12 @@
 modded class PluginRecipesManager 
 {
-     override void RegisterRecipies()
+    override void RegisterRecipies()
     {
         super.RegisterRecipies();
         UnregisterRecipe("craft_mmg_crate03_unfinished_wood");
         UnregisterRecipe("craft_mmg_crate03_wood");
         UnregisterRecipe("craft_mmg_crate03_grey");
-        UnregitserRecipe("craft_mmg_gun_wall_kit_wooden");
+        UnregisterRecipe("craft_mmg_gun_wall_kit_wooden");
         UnregisterRecipe("craft_mmg_gun_wall_kit_metal");
         UnregisterRecipe("craft_mmg_gun_rack_planks");
         UnregisterRecipe("craft_mmg_craftable_gun_rack");
